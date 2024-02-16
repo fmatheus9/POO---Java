559d622bbda1f3f1e5ce7051ff5fb0fa1ffd68bf
Matheus Ferrarezi <matheus.fe.m@gmail.com>
2024-02-16 13:00:54 -0300

    Create Main.java

---
+++ Curso em Vídeo/Controle/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Controle c1 = new Controle();
+        c1.ligar();
+        c1.play();
+        c1.maisVolume();
+        c1.abrirMenu();
+    }
+
+}
