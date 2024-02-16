a9f370493c032a5ddc6066bf559d1d70fab52588
Matheus Ferrarezi <matheus.fe.m@gmail.com>
2024-02-16 13:01:34 -0300

    Create InterfaceControle.java

---
+++ Curso em Vídeo/Controle/InterfaceControle.java
@@ -0,0 +1,12 @@
+public interface InterfaceControle {
+    abstract void ligar();
+    abstract void desligar();
+    abstract void abrirMenu();
+    abstract void fecharMenu();
+    abstract void maisVolume();
+    abstract void menosVolume();
+    abstract void ligarMudo();
+    abstract void desligarMudo();
+    abstract void play();
+    abstract void pause();
+}
