7608be57cd1f98c4a56b0dfbeacc50a24929cbf6
Matheus Ferrarezi <matheus.fe.m@gmail.com>
2024-02-16 12:58:59 -0300

    Create Main.java

---
+++ Curso em Vídeo/Caneta/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta(); //cria o objeto
+        c1.modelo = "Bic";  // Chamada ao atributo
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 100;
+        //c1.tampa = false;
+        c1.destampar();
+        c1.status(); //referencia ao método
+        c1.rabiscar();
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "hostnet";
+        c2.cor = "Preto";
+        c2.ponta = 0.7f;
+        c2.carga = 90;
+        c2.tampar();
+        c2.status();
+        c2.rabiscar();
+    }
+}
