fd922d555667c57f5f269c990f8c9b7b713254b8
Matheus Ferrarezi <matheus.fe.m@gmail.com>
2024-02-19 18:02:54 -0300

    Create Publicação.java

---
+++ Curso em Vídeo/Pessoa/Publicação.java
@@ -0,0 +1,8 @@
+public interface Publicação {
+    abstract void abrir();
+    abstract void fechar();
+    abstract void folhear();
+    abstract void avançarPagina();
+    abstract void voltarPagina();
+
+}
